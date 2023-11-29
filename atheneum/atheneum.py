76f06c22cc12da72ae44b34b02b96749da418a04
Tanaya Mankad <tanaya.mankad@bigladdersoftware.com>
2023-11-29 12:48:57 -0700

    Fix pylint errors and actions matrix.

--- atheneum/atheneum.py
+++ atheneum/atheneum.py
@@ -1,3 +1,17 @@
-class Atheneum:
-    def answer(self):
+"""A boilerplate module for Python packate/library design"""
+
+class Atheneum: #pylint:disable=R0903
+    """The atheneum class has all the answers"""
+
+    def __init__(self): #pylint:disable=C0116
+        pass
+
+    def answer(self, exclamation_str):
+        """
+        Return the answer to Life, the Universe, and Everything.
+
+        :param exclamation_str:     Print how you feel about the answer
+        """
+
+        print(exclamation_str)
         return 42
